10660ec3acc58af2f23bf771db2a33b20982d23c
Allie Kim <allie0147@gmail.com>
2022-06-08 23:42:07 +0900

    Feat: Bottom bar & App icon

--- lib/main.dart
+++ lib/main.dart
@@ -11,76 +11,103 @@ class MyApp2 extends StatefulWidget {
 }
 
 class _MyApp2State extends State<MyApp2> {
+  @override
+  Widget build(BuildContext context) {
+    // every widget contains arguments
+    return const MaterialApp(
+      home: FirstPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class FirstPage extends StatefulWidget {
+  const FirstPage({Key? key}) : super(key: key);
+
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
   String buttonName = 'Click!!';
+  String nextPageName = 'Next page';
   int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    // every widget contains arguments
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.pink,
-          title: const Text('타이틀'),
-        ),
-        body: Center(
-            child: currentIndex == 0
-                ? Container(
-                    color: Colors.orange,
-                    width: double.infinity, // full width
-                    height: double.infinity,
-                    child: Row(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                onPrimary: Colors.white, primary: Colors.pink),
-                            onPressed: () {
-                              setState(() {
-                                // setState(){} => notice app widget is going to be changed
-                                buttonName = 'Clicked~!';
-                              });
-                            },
-                            child: Text(buttonName)),
-                        ElevatedButton(
-                            onPressed: () {
-                              setState(() {
-                                // setState(){} => notice app widget is going to be changed
-                                buttonName = 'Clicked~!';
-                              });
-                            },
-                            child: Text(buttonName)),
-                      ],
-                    ),
-                  )
-                : Image.asset('images/fire.jpeg')),
-        bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: Colors.pink,
-          unselectedItemColor: Colors.white70,
-          fixedColor: Colors.white,
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.home,
-                size: 24,
-              ),
-              label: 'Home',
-              activeIcon: Icon(Icons.home),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pink,
+        title: const Text('타이틀'),
+      ),
+      body: Center(
+          child: currentIndex == 0
+              ? Container(
+                  color: Colors.orange,
+                  width: double.infinity, // full width
+                  height: double.infinity,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              onPrimary: Colors.white, primary: Colors.pink),
+                          onPressed: () {
+                            setState(() {
+                              // setState(){} => notice app widget is going to be changed
+                              buttonName = 'Clicked~!';
+                            });
+                          },
+                          child: Text(buttonName)),
+                      ElevatedButton(
+                          onPressed: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => const SecondPage()));
+                          },
+                          child: Text(nextPageName)),
+                    ],
+                  ),
+                )
+              : Image.asset('images/fire.jpeg')),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.pink,
+        unselectedItemColor: Colors.white70,
+        fixedColor: Colors.white,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              size: 24,
             ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.settings), label: 'Settings')
-          ],
-          onTap: (int index) {
-            setState(() {
-              currentIndex = index;
-            });
-          },
-          currentIndex: currentIndex,
-        ),
-      ), // home argument
-      debugShowCheckedModeBanner: false,
+            label: 'Home',
+            activeIcon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings')
+        ],
+        onTap: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        currentIndex: currentIndex,
+      ),
+    ); // home argument
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('타이틀2'),
+        backgroundColor: Colors.pink,
+        automaticallyImplyLeading: true,
+      ),
     );
   }
 }
